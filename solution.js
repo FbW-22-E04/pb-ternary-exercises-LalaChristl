0ca3d905289f8e79ec6e204c7fda72cb314a02ed
LalaChristl <lesleyannchristl@gmail.com>
2022-07-11 17:39:59 +0200

    added JS

---
+++ solution.js
@@ -0,0 +1,36 @@
+ const isDog = true;
+ console.log(isDog == true ? 'pat, pat' : 'find me a dog to pat!');
+console.log('----------------------------------------');
+
+ const speedLimit = 40;
+ const yourSpeed = 90;
+ console.log(yourSpeed > speedLimit ? "you're going too fast!" : "You're driving below the speed limit, Oma");
+
+ console.log('----------------------------------------');
+
+ const age = 23;
+ console.log(age < 16 ? 'serve butter beer' : 'serve beer');
+
+ console.log('----------------------------------------');
+
+ const isStudent = false;
+ console.log(isStudent == true ? 'Ticket costs €5,00' : 'Ticket costs €12,00');
+
+ console.log('----------------------------------------');
+ 
+ const okMarie = 'cookies';
+ console.log(okMarie == 'cake' ? 'Let them eat cake' : 'Oh, bother');
+
+ console.log('----------------------------------------');
+
+const num1 = 30
+const num2 = 939
+const num3 = 40.9
+
+console.log(num1 % 2 === 0 ? `${num1} is even`: `${num1} is odd`);
+
+console.log(num2 % 2 === 0 ? `${num2} is even`: `${num2} is odd`);
+
+console.log(num3 % 2 === 0 ? `${num3} is even`: `${num3} is odd`);
+
+
